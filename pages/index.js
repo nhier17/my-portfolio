4569b2424e4eb21b6bad101db8f00b01d7040181
Abraham <abrahamnhier97@gmail.com>
2024-01-04 18:34:09 +0300

    resume

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ export default function Home() {
       <BsFillMoonStarsFill onClick={()=>setDarkMode(!darkMode)} className='cursor-pointer text-2xl'/>
     </li>
     <li>
-      <a className=' bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href='#'>Resume</a>
+      <a className=' bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href='https://www.linkedin.com/in/abraham-nhier-369308291/' target='_blank'>Resume</a>
       </li>
   </ul>
 </nav>
