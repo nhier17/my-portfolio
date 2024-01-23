618468bafbac0519000aa8649e1041385da2eba3
Abraham <abrahamnhier97@gmail.com>
2024-01-23 09:24:59 +0300

    more updates

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ export default function Home() {
 </nav>
 <div className="text-center p-10  dark:bg-white">
   <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">Abraham Nhier</h2>
-  <h3 className="text-2xl py-2 md:text-3xl">Developer and designer.</h3>
+  <h3 className="text-2xl py-2 md:text-3xl">Developer .</h3>
   <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto">
   I am a software developer passionate about coding and problem solving with a solid foundation in both front-end and back-end technologies.
 So far I've developed responsive and intuitive user interfaces  using technologies and libraries like Javascript, ReactJs, HTML and CSS. 
